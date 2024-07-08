eed9da05a2b91aa9a2252cf8e735594b072fa5b7
AlexD4121 <alex.deane@uconn.edu>
2024-07-07 23:02:46 -0400

    Set up Initial nesting inside of Model and Friends Controller

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,2 +1,9 @@
 class Friend < ApplicationRecord
+  has_many :gifts, dependent: :destroy, inverse_of: :friend
+  accepts_nested_attributes_for  :gifts, allow_destroy: true, reject_if: :all_blank
+
+
+  # Add validations for presence of name and birthday
+  validates :name, presence: true
+  validates :birthday, presence: true
 end
--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,3 +1,7 @@
 class Gift < ApplicationRecord
   belongs_to :friend
+
+  validates :name, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
+  validates :link, presence: true
 end
